a6894f932ac3cd2e4e05b2f6dff568c818b536e3
Thesupersupersigma <thesupersupersigma@gmail.com>
2026-08-11 18:07:26 -0400

    added 1 photo to public for later use, and added first js file and used it on 404.html

---
+++ src/app/TSSS.js
@@ -0,0 +1,17 @@
+// This is my first js file so its gonna be ass so bear w me pls
+
+console.log("thesupersupersigma was here");
+console.log("Im planning on adding some ascii art here but at the moment i dont have wifi bcus of a stupid pwr outage");
+
+
+
+/*
+--------------   | ----
+      |           / finish this
+      |
+      |
+      |
+      |
+      |
+      |
+*/
